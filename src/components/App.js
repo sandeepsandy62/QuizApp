669c32225382a96912d57591b699f8eba5d5fb5f
sandeepsandy62 <sandeepgogarla649@gmail.com>
2023-08-19 03:05:25 +0530

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,137 @@
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import { useEffect, useReducer } from "react";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+import NextButton from "./NextButton";
+import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
+import "../index.css";
+
+const initialState = {
+  questions: [],
+
+  //'loading' , 'error' , 'ready' , 'active' , 'finished'
+  status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+
+    case "start":
+      return {
+        ...state,
+        status: "active",
+      };
+
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+
+    case "nextQuestion":
+      return {
+        ...state,
+        index: state.index + 1,
+        answer: null,
+      };
+
+    case "finish":
+      return {
+        ...state,
+        status:"finished",
+        index:state.index+1,
+      };
+
+    default:
+      throw new Error("Action Unknown");
+  }
+}
+
+function App() {
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
+
+  useEffect(function () {
+    fetch("http://localhost:9005/questions")
+      .then((res) =>
+        res
+          .json()
+          .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      )
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  return (
+    <div className="app">
+      {/* Header */}
+      <Header />
+
+      {/* Main */}
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
+            <Question
+              question={questions[index]}
+              answer={answer}
+              dispatch={dispatch}
+            />
+          </>
+        )}
+        <NextButton 
+        dispatch={dispatch}
+        answer={answer}
+        index={index}
+        numQuestions={numQuestions} />
+        {status === "finished" && (
+          <FinishScreen points={points} maxPossiblePoints={maxPossiblePoints} />
+        )}
+      </Main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FinishScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function FinishScreen({points,maxPossiblePoints}) {
+  const percentage = (points / maxPossiblePoints) * 100;
+    return (
+    <p className='result'>
+        You Scored <strong>{points}</strong> out of {maxPossiblePoints} {Math.ceil(percentage)}
+    </p>
+  )
+}
